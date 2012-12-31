b761a48ecb07dacceb0955e07eac5a32c75d1ede
Yak! <yak_ex@mx.scn.tv>
2012-12-31 20:42:59 +0900

    Make configuration dialog.

--- resource.h
+++ resource.h
@@ -13,10 +13,19 @@
 #ifndef RESOURCE_H
 #define RESOURCE_H
 
-#define IDD_ABOUT_DIALOG               101
-#define IDD_CONFIG_DIALOG              102
-#define IDC_DONT_DUPLICATE             1001
-#define IDC_STATIC                     0
-#define IDV_VERSIONINFO                1
+#define IDD_ABOUT_DIALOG                101
+#define IDD_CONFIG_DIALOG               102
+
+#define IDV_VERSIONINFO                 1
+
+#define IDC_STATIC                      0
+#define IDC_EDIT_FFPROBE_PATH           1000
+#define IDC_BROWSE_FFPROBE              1001
+#define IDC_EDIT_FFMPEG_PATH            1002
+#define IDC_BROWSE_FFMPEG               1003
+#define IDC_RADIO_IMAGES                1004
+#define IDC_RADIO_INTERVAL              1005
+#define IDC_EDIT_NUMBER                 1006
+#define IDC_SPIN_NUMBER                 1007
 
 #endif
