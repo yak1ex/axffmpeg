1e8268be5f6dd84fe752e46816e7de1fbf8b2291
Yak! <yak_ex@mx.scn.tv>
2013-02-04 23:50:00 +0900

    Add *.ogm as supported extension.

--- axffmpeg.cpp
+++ axffmpeg.cpp
@@ -62,7 +62,7 @@ static bool g_fProgress;
 const char* table[] = {
 	"00AM",
 	"Plugin to handle a movie as an image container - v0.02 (2013/01/22) Written by Yak!",
-	"*.3g2;*.3gp;*.avi;*.f4v;*.flv;*.m4v;*.mkv;*.mov;*.mp4;*.mpeg;*.mpg;*.ogg;*.webm;*.wmv",
+	"*.3g2;*.3gp;*.avi;*.f4v;*.flv;*.m4v;*.mkv;*.mov;*.mp4;*.mpeg;*.mpg;*.ogg;*.ogm;*.webm;*.wmv",
 	"“®‰æƒtƒ@ƒCƒ‹"
 };
 
